c8075ff6e4a80f8df45b5543a3b9d92d5060dac1
Jeramy Sena <jeramy_sena@hotmail.com>
2021-12-09 14:08:55 -0600

    add employee list

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employees (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  department VARCHAR(30) NOT NULL,
+  manager VARCHAR(30) NOT NULL
+); 
+ 
---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO employees (first_name, last_name, department, manager)
+VALUES
+('Bob', 'Young', 'Sales', 'Evans'),
+('Jeff', 'King', 'Engineeriner', 'Martin'),
+('Alice', 'Anderson', 'Production', 'Keller'),
+('Frank', 'Williams', 'Maintenance', 'Carter'),
+('Paula', 'Martinez', 'Development', 'Martin'),
+('Carl', 'Sanders', 'Sales', 'Evans'),
+('Janet', 'Longmire', 'Engineer', 'Martin'),
+('Scott', 'James', 'Maintenance', 'Carter'),
+('Shawna', 'Potts', 'Manager', 'Stevens'),
+('Dallas', 'Tolbert', 'Front Desk', 'Potts');
+
+
